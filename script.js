74fc661d02c8ba562178f672c44eb3452dc483b7
Ilia Lazarev <kephircheek@gmail.com>
2024-09-30 08:23:28 +0300

    Обновляет логику приложения

--- script.js
+++ script.js
@@ -1,99 +1,281 @@
-let text = localStorage.getItem('text');
-let words = text.split(/\s+/);
-let currentIndex = -1;
+document
+    .getElementById("textDisplay")
+    .appendChild(renderHTML(parseText(localStorage.getItem("text"))));
 
-const textDisplay = document.getElementById('textDisplay');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-let currentWordElement = null;
-let currentWordSyllables = [];
-let syllableIndex = 0;
+const ReaderState = Object.freeze({
+    NO_FOCUS: 0,
+    PARAGRAPH_FOCUS: 1,
+    LINE_FOCUS: 2,
+    WORD_FOCUS: 3,
+    FRAGMENT_FOCUS: 4,
+});
 
-function displayText() {
-    textDisplay.innerHTML = words.map((word, index) => 
-        `<span class="word" data-index="${index}">${word}</span>`
-    ).join(' ');
-}
+let readerState = ReaderState.NO_FOCUS;
+let focusedWordId = null;
+let focusedWordForms = null;
+let focusedWordFormNumber = 0;
 
-function highlightWord(index) {
-    if (currentWordElement) {
-        currentWordElement.classList.remove('highlighted-word');
-    }
-
-    const wordsSpans = document.querySelectorAll('.word');
-    currentWordElement = wordsSpans[index];
-    currentWordElement.classList.add('highlighted-word');
-
-    wordsSpans.forEach((span, i) => {
-        if (i !== index) {
-            span.classList.add('dimmed-text');
-        } else {
-            span.classList.remove('dimmed-text');
-        }
+function parseText(text) {
+    const paragraphs = text.trim().split(/\n\s*\n/);
+    return paragraphs.map((paragraph) => {
+        return paragraph.split(/\s*\n/).map((line) => line.split(/\s+/));
     });
-
-    // Расчёт слогов для текущего слова
-    currentWordSyllables = splitIntoSyllables(words[index]);
-    syllableIndex = 0;
 }
 
-function splitIntoSyllables(word) {
-    let vowels = 'аеёиоуыэюя';
-    let syllables = [];
-    let syllable = '';
-
-    for (let i = 0; i < word.length; i++) {
-        let char = word[i];
-        syllable += char;
-
-        if (vowels.includes(char.toLowerCase()) || (i > 0 && !vowels.includes(word[i - 1].toLowerCase()))) {
-            syllables.push(syllable);
-            syllable = '';
-        }
-    }
-
-    if (syllable) {
-        syllables.push(syllable);
-    }
-    return syllables;
-}
-
-function showNextStep() {
-    if (syllableIndex < currentWordSyllables.length) {
-        currentWordElement.innerHTML = currentWordSyllables
-            .map((syllable, index) => 
-                index === syllableIndex ? `<span class="highlighted-word">${syllable}</span>` : syllable)
-            .join('-');
-        syllableIndex++;
-    } else {
-        highlightWord(++currentIndex);
-    }
-}
-
-function showPreviousStep() {
-    // Логика обратного хода (аналогична showNextStep, но для обратного направления)
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    displayText();
-
-    document.querySelectorAll('.word').forEach((span) => {
-        span.addEventListener('click', function () {
-            currentIndex = parseInt(this.getAttribute('data-index'));
-            highlightWord(currentIndex);
+function renderHTML(parsedText) {
+    const container = document.createElement("article");
+    parsedText.forEach((paragraph, paragraphIndex) => {
+        const paragraphElement = document.createElement("p");
+        const paragraphGlobalIndex = `${paragraphIndex}`;
+        paragraphElement.setAttribute("id", paragraphGlobalIndex);
+        paragraphElement.classList.add("paragraph");
+        paragraph.forEach((line, lineIndex) => {
+            const lineElement = document.createElement("span");
+            const lineGlobalIndex = `${paragraphGlobalIndex}.${lineIndex}`;
+            lineElement.setAttribute("id", lineGlobalIndex);
+            lineElement.classList.add("line");
+            line.forEach((word, wordIndex) => {
+                const wordElement = document.createElement("span");
+                const wordGlobalIndex = `${lineGlobalIndex}.${wordIndex}`;
+                wordElement.classList.add("word");
+                wordElement.setAttribute("id", wordGlobalIndex);
+                wordElement.addEventListener("click", () => {
+                    focusWord(wordGlobalIndex);
+                });
+                const [pureWord, punctuation] = splitWordAndPunctuation(word);
+                wordElement.textContent = pureWord;
+                lineElement.appendChild(wordElement);
+                if (punctuation !== "") {
+                    const punctuationNode =
+                        document.createTextNode(punctuation);
+                    lineElement.appendChild(punctuationNode);
+                }
+                if (wordIndex === line.length - 1) {
+                    const brElement = document.createElement("br");
+                    lineElement.appendChild(brElement);
+                } else {
+                    const spaceNode = document.createTextNode(" ");
+                    lineElement.appendChild(spaceNode);
+                }
+            });
+            paragraphElement.appendChild(lineElement);
         });
+        container.appendChild(paragraphElement);
     });
+    return container;
+}
 
-    nextBtn.addEventListener('click', showNextStep);
-    prevBtn.addEventListener('click', showPreviousStep);
-});
+function getLineIndex(wordIndex) {
+    return wordIndex.split(".").slice(0, 2).join(".");
+}
 
-document.getElementById('startReading').addEventListener('click', function() {
-    const text = document.getElementById('inputText').value;
-    if (text.trim()) {
-        localStorage.setItem('text', text);
-        window.location.href = 'reader.html';
-    } else {
-        alert('Введите текст.');
+function getParagraphIndex(wordIndex) {
+    return wordIndex.split(".").slice(0, 1).join(".");
+}
+
+function focusByID(id) {
+    const element = document.getElementById(id);
+    element.classList.add("focused-text");
+}
+
+function unFocusById(id) {
+    const element = document.getElementById(id);
+    element.classList.remove("focused-text");
+}
+
+function restoreFocusedWord() {
+    const wordElement = document.getElementById(focusedWordId);
+    wordElement.textContent = focusedWordForms[0];
+}
+
+function unFocusReader() {
+    switch (readerState) {
+        case ReaderState.NO_FOCUS:
+            break;
+        case ReaderState.FRAGMENT_FOCUS:
+            restoreFocusedWord();
+            break;
+        case ReaderState.WORD_FOCUS:
+            unFocusById(focusedWordId);
+            break;
+        case ReaderState.LINE_FOCUS:
+            unFocusById(getLineIndex(focusedWordId));
+            break;
+        case ReaderState.PARAGRAPH_FOCUS:
+            unFocusById(getParagraphIndex(focusedWordId));
     }
-});
+}
+
+function focusWord(id) {
+    if (id === focusedWordId) {
+        return;
+    }
+    unFocusReader();
+    readerState = ReaderState.WORD_FOCUS;
+    focusedWordId = id;
+    focusedWordForms = generateWordForms(
+        document.getElementById(id).textContent,
+    );
+    console.log("Id:", id, "forms:", focusedWordForms);
+    focusByID(id);
+}
+
+function focusNextWord() {
+    const [paragraphId, lineId, wordId] = focusedWordId.split(".").map(Number);
+    let globalWordId = [paragraphId, lineId, wordId + 1].map(String).join(".");
+    if (document.getElementById(globalWordId) === null) {
+        globalWordId = [paragraphId, lineId + 1, 0].map(String).join(".");
+        if (document.getElementById(globalWordId) === null) {
+            globalWordId = [paragraphId + 1, 0, 0].map(String).join(".");
+            if (document.getElementById(globalWordId) === null) {
+                focusedWordId = null;
+            }
+        }
+    }
+    focusWord(globalWordId);
+}
+
+function focusPreviousWord() {
+    const [paragraphId, lineId, wordId] = focusedWordId.split(".").map(Number);
+    let globalWordId = [paragraphId, lineId, wordId - 1].map(String).join(".");
+    if (document.getElementById(globalWordId) === null) {
+        globalWordId = [paragraphId, lineId - 1, 0].map(String).join(".");
+        if (document.getElementById(globalWordId) === null) {
+            globalWordId = [paragraphId - 1, 0, 0].map(String).join(".");
+            if (document.getElementById(globalWordId) === null) {
+                focusedWordId = null;
+            }
+        }
+    }
+    focusWord(globalWordId);
+}
+
+function splitWordAndPunctuation(str) {
+    const match = str.trim().match(/(\p{L}+)(\W*)/u);
+    return [match[1], match[2]];
+}
+
+function splitOnFragments(word) {
+    const [fragment, remains] = splitOffOneFragment(word);
+    if (remains.length === 0) {
+        return [fragment];
+    }
+    let fragments = splitOnFragments(remains);
+    fragments.unshift(fragment);
+    return fragments;
+}
+
+function splitOffOneFragment(word) {
+    const vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯъьЬЪ";
+    if (vowels.includes(word[0])) {
+        return [word[0], word.slice(1)];
+    }
+    if (!vowels.includes(word[1])) {
+        return [word[0], word.slice(1)];
+    }
+    return [word.slice(0, 2), word.slice(2)];
+}
+
+function generateWordForms(word) {
+    let forms = [word];
+    const fragments = splitOnFragments(word);
+    console.log(fragments);
+    if (fragments.length === 1) {
+        return forms;
+    }
+    for (let i = 0; i < fragments.length; i++) {
+        switch (i) {
+            case 0:
+                forms.push([
+                    null,
+                    fragments[i],
+                    fragments.slice(i + 1).join(""),
+                ]);
+                break;
+            case fragments.length - 1:
+                forms.push([
+                    fragments.slice(0, i).join(""),
+                    fragments[i],
+                    null,
+                ]);
+                break;
+            default:
+                forms.push([
+                    fragments.slice(0, i).join(""),
+                    fragments[i],
+                    fragments.slice(i + 1).join(""),
+                ]);
+                forms.push([
+                    null,
+                    fragments.slice(0, i + 1).join(""),
+                    fragments.slice(i + 1).join(""),
+                ]);
+                break;
+        }
+    }
+    forms.push([null, word, null]);
+    return forms;
+}
+
+function focusFragment() {
+    readerState = ReaderState.FRAGMENT_FOCUS;
+    let [waited, focused, remains] = focusedWordForms[focusedWordFormNumber];
+    let wordElement = document.getElementById(focusedWordId);
+    wordElement.innerHTML = "";
+    if (waited !== null) {
+        let waitedElement = document.createElement("span");
+        waitedElement.textContent = waited + "-";
+        wordElement.appendChild(waitedElement);
+    }
+    let focusedElement = document.createElement("span");
+    focusedElement.textContent = focused;
+    focusedElement.classList.add("focused-text");
+    wordElement.appendChild(focusedElement);
+    if (remains !== null) {
+        const remainsElement = document.createTextNode("-" + remains);
+        wordElement.appendChild(remainsElement);
+    }
+}
+
+function forward() {
+    unFocusReader();
+    switch (readerState) {
+        case ReaderState.NO_FOCUS:
+            focusWord("0.0.0");
+            break;
+        case ReaderState.WORD_FOCUS:
+            if (focusedWordForms.length === 1) {
+                focusNextWord();
+                break;
+            }
+            focusedWordFormNumber = 1;
+            focusFragment();
+            break;
+        case ReaderState.FRAGMENT_FOCUS:
+            focusedWordFormNumber++;
+            if (focusedWordFormNumber === focusedWordForms.length) {
+                focusNextWord();
+                break;
+            }
+            focusFragment();
+            break;
+    }
+}
+
+function rewind() {
+    unFocusReader();
+    switch (readerState) {
+        case ReaderState.NO_FOCUS:
+            break;
+        case ReaderState.WORD_FOCUS:
+            focusPreviousWord();
+        case ReaderState.FRAGMENT_FOCUS:
+            focusedWordFormNumber--;
+            if (focusedWordFormNumber === 0) {
+                focusPreviousWord();
+                break;
+            }
+            focusFragment();
+            break;
+    }
+}
