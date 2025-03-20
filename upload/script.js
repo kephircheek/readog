0688724cd9beb05013c68daa9d77bae53c06ce33
Ilia Lazarev <kephircheek@gmail.com>
2025-03-20 09:13:34 +0300

    Выделяет загрузчик в отдельный модуль 'upload'

---
+++ upload/script.js
@@ -0,0 +1,73 @@
+navigator.clipboard
+    .readText()
+    .then(text => {
+        document.getElementById("textInput").value = text;
+    })
+    .catch(err => {
+        console.error("Ошибка при чтении из буфера обмена:", err);
+    });
+
+function startReading() {
+    const text = document.getElementById("textInput").value;
+    if (text.trim()) {
+        localStorage.setItem("text", text);
+        window.location.href = "../reader/index.html";
+    } else {
+        alert("Введите текст!");
+    }
+}
+
+function showSpinner() {
+    document.getElementById('spinner').classList.add('show');
+    [
+        document.getElementById('startReading'),
+        document.getElementById('textInput'),
+        document.getElementById('imageInputLabel')
+    ].forEach((elem) => {
+        ['dimmed-text', 'unresponsive'].forEach((cls) => {
+            elem.classList.add(cls);
+        });
+    });
+}
+
+function hideSpinner() {
+    document.getElementById('spinner').classList.remove('show');
+    [
+        document.getElementById('startReading'),
+        document.getElementById('textInput'),
+        document.getElementById('imageInputLabel')
+    ].forEach((elem) => {
+        ['dimmed-text', 'unresponsive'].forEach((cls) => {
+            elem.classList.remove(cls);
+        });
+    });
+}
+
+document.getElementById('imageInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = new Image();
+            img.src = e.target.result;
+            showSpinner();
+            Tesseract.recognize(img.src, 'rus', {
+                logger: m => console.log(m)
+            }).then(({
+                data: {
+                    text
+                }
+            }) => {
+                const textarea = document.getElementById("textInput");
+                textarea.value += text;
+                textarea.selectionStart = textarea.selectionEnd = textarea.value.length;
+                textarea.scrollTop = textarea.scrollHeight;
+                hideSpinner();
+            }).catch(err => {
+                hideSpinner();
+                alert('Ошибка распознавания текста');
+            });
+        };
+        reader.readAsDataURL(file);
+    }
+});
