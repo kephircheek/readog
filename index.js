9f9dfa556bcadcdb621a216bc6d1a5ad705b53bc
Ilia Lazarev <kephircheek@gmail.com>
2025-03-23 22:55:05 +0300

    Обновляет список на homepage при каждом показе
    Включая возвращение назад на homepage

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const history = JSON.parse(localStorage.getItem(storyHistoryKey())) || [];
     insertStories("", "recent-stories", history);
 });
+
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) {
+        const history = JSON.parse(localStorage.getItem(storyHistoryKey())) || [];
+        insertStories("", "recent-stories", history);
+    }
+});
